e0f7d5b630a52f72c0c1b27ea1fdc0940a465620
anastasyah86 <nastasyah86@gmail.com>
2021-01-25 14:57:28 +0200

    refs + class

--- src/index.js
+++ src/index.js
@@ -1,8 +1,40 @@
 import './styles.css';
+
+const refs = {
+  days: document.querySelector('span[data-value="days"]'),
+  hours: document.querySelector('span[data-value="hours"]'),
+  mins: document.querySelector('span[data-value="mins"]'),
+  secs: document.querySelector('span[data-value="secs"]'),
+};
+
+class CountdownTimer {
+  constructor(selector, targetDate) {
+    this.selector = selector;
+    this.targetDate = targetDate;
+  }
+
+  start() {
+    const startTime = Date.now();
+    this.targetDate = setInterval(() => {
+      const currentTime = Date.now();
+      const difference = startTime - currentTime;
+      updateClockface(difference);
+    }, 1000);
+  }
+
+  stop() {
+    clearInterval(this.intervalId);
+    this.intervalId = null;
+    this.isActive = false;
+    updateClockface(0);
+  }
+}
+
 new CountdownTimer({
   selector: '#timer-1',
   targetDate: new Date('Jul 17, 2019'),
 });
+
 const days = Math.floor(time / (1000 * 60 * 60 * 24));
 const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
