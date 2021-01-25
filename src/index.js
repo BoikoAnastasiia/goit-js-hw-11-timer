66fcf66fe6ec21be6b9eecbe11910237e6d44cc5
anastasyah86 <nastasyah86@gmail.com>
2021-01-25 15:27:37 +0200

    class done

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const refs = {
   hours: document.querySelector('span[data-value="hours"]'),
   mins: document.querySelector('span[data-value="mins"]'),
   secs: document.querySelector('span[data-value="secs"]'),
+  timer: document.querySelector('#timer-1'),
 };
 
 class CountdownTimer {
@@ -12,21 +13,36 @@ class CountdownTimer {
     this.selector = selector;
     this.targetDate = targetDate;
   }
-
+  selector() {
+    this.selector = refs.timer;
+  }
   start() {
     const startTime = Date.now();
     this.targetDate = setInterval(() => {
       const currentTime = Date.now();
-      const difference = startTime - currentTime;
-      updateClockface(difference);
+      const time = startTime - currentTime;
+      updateClockface(time);
     }, 1000);
   }
 
-  stop() {
-    clearInterval(this.intervalId);
-    this.intervalId = null;
-    this.isActive = false;
-    updateClockface(0);
+  updateClockface(time) {
+    const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+    );
+    const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+    refs.days.textContent = `${days}`;
+    refs.hours.textContent = `${hours}`;
+    refs.mins.textContent = `${mins}`;
+    refs.secs.textContent = `${secs}`;
+    // console.log(`${hours}:${mins}:${secs}`);
+  }
+  pad(value) {
+    if (value.lenght > 2) {
+      String(value).padStart(3, '0');
+    }
+    String(value).padStart(2, '0');
   }
 }
 
@@ -34,8 +50,3 @@ new CountdownTimer({
   selector: '#timer-1',
   targetDate: new Date('Jul 17, 2019'),
 });
-
-const days = Math.floor(time / (1000 * 60 * 60 * 24));
-const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-const secs = Math.floor((time % (1000 * 60)) / 1000);
